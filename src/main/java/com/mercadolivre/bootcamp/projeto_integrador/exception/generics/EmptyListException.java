36e4081d4fc8373803759103261b4445dfe5aecf
Rafael Santana <rafael.santana@mercadolivre.com>
2022-04-27 17:33:44 -0300

    Create EmptyListException Class

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/exception/generics/EmptyListException.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/exception/generics/EmptyListException.java
@@ -1,4 +1,13 @@
 package com.mercadolivre.bootcamp.projeto_integrador.exception.generics;
 
-public class EmptyListException {
+import com.mercadolivre.bootcamp.projeto_integrador.exception.BaseException;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public class EmptyListException extends BaseException {
+
+    public EmptyListException() {
+        super("Não foi encontrato nenhum registro", HttpStatus.NOT_FOUND, ZonedDateTime.now());
+    }
 }
